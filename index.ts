7c7a40337fd851e94e50a5b8122f64c2f92c070f
syedmaazhasan0 <160649717+syedmaazhasan0@users.noreply.github.com>
2024-03-24 00:05:15 -0700

    Created Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk";
+
+// computer will generate a random number - done
+// user will guess a random number - done
+// comparing user no with origional no and shoe result - done
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10",
+    }
+]);
+if(answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.green.italic("Congratulations you guessed the right number"));
+    } else{
+        console.log(chalk.red.bold.underline("Oops you guess the wrong number! Try again"));
+        
+    }
+
